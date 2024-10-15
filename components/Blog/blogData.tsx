b9ed1d15728e83d84436196848b8dca9f3a5c7f9
AmodLupane <amodlupane1706@gmail.com>
2024-10-15 20:46:02 +0530

    Initial Commit

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -4,7 +4,7 @@ const BlogData: Blog[] = [
   {
     _id: 1,
     mainImage: "/images/blog/blog-01.png",
-    title: "Free advertising for your online business",
+    title: "E-commerce Templates",
     metadata:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
   },
